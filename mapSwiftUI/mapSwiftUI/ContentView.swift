013f9f3e0b9c262385a36c06cd6ff01d92b0b4af
devAshwaq <88581485+engAshwaqAlmunmms@users.noreply.github.com>
2024-03-08 01:26:04 +0300

    set initial camera position

--- mapSwiftUI/mapSwiftUI/ContentView.swift
+++ mapSwiftUI/mapSwiftUI/ContentView.swift
@@ -9,11 +9,26 @@ import SwiftUI
 import  MapKit
 
 struct ContentView: View {
+    
     //MARK :- 24.7136° N, 46.6753° E , Riyadh/Coordinates
     
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 24.7136, longitude: 46.6753),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
     
     var body: some View {
         VStack {
+            Text("Map In SwiftUI With Ashwaq !")
+                .bold()
+                .padding([.top], 10)
+            Text("we use startPosition for initial camera position")
+                .padding([.top], 10)
+                .foregroundColor(.orange)
+            Map(initialPosition: startPosition)
+                .mapStyle(.hybrid)
         }
         .padding()
     }
