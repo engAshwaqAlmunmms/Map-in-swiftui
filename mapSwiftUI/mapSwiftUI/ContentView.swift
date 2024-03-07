79963fe36f4a51289e3e1d7d02718f46cee891ef
devAshwaq <88581485+engAshwaqAlmunmms@users.noreply.github.com>
2024-03-08 00:54:12 +0300

    create new project
    create new project

---
+++ mapSwiftUI/mapSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mapSwiftUI
+//
+//  Created by Ashwaq Alghamdi on 8.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mapSwiftUI/mapSwiftUI/mapSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapSwiftUIApp.swift
+//  mapSwiftUI
+//
+//  Created by Ashwaq Alghamdi on 8.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct mapSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
