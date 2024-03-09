a46ef1245fda56492911e05f61ad1ff4cdea488c
devAshwaq <88581485+engAshwaqAlmunmms@users.noreply.github.com>
2024-03-09 03:44:17 +0300

    new map

---
+++ mapSwiftUI/mapSwiftUI/ContentView2.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView2.swift
+//  mapSwiftUI
+//
+//  Created by Ashwaq Alghamdi on 9.03.2024.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView2: View {
+    
+    var body: some View {
+        VStack {
+            Text("Map In SwiftUI With Ashwaq !")
+                .bold()
+                .padding([.bottom], 50)
+        }
+    }
+}
+
+#Preview {
+    ContentView2()
+}
